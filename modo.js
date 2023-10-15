5ec60033a2d2fd7b8ac06ad3e96d14151f08e578
natrium <130921628+naatrium@users.noreply.github.com>
2023-10-15 17:22:17 -0300

    Update modo.js

--- modo.js
+++ modo.js
@@ -42,7 +42,56 @@ function darkMode() {
     starAnterior = aux;
 }
 
-   
-  
+// Referenciamos os botões e conteúdos com as classes correspondentes
+const toggleButtons = document.querySelectorAll(".toggleButton");
+const conteudos = document.querySelectorAll(".conteudo");
 
-  
+// Adicionamos um ouvinte de evento de clique a cada botão
+toggleButtons.forEach((toggleButton, index) => {
+  toggleButton.addEventListener("click", function() {
+    // Verificamos o estado de exibição do conteúdo correspondente
+    if (conteudos[index].style.display === "none" || conteudos[index].style.display === "") {
+      // Se estiver oculto, mostramos
+      conteudos[index].style.display = "block";
+    } else {
+      // Se estiver visível, ocultamos
+      conteudos[index].style.display = "none";
+    }
+  });
+});
+
+
+
+
+document.getElementById("botaoSalvar").addEventListener("click", function() {
+  // Pega o texto da área de texto
+  var resenha = document.getElementById("resenha").value;
+
+  if (resenha.trim() === "") {
+      alert("Por favor, escreva uma resenha antes de salvar.");
+      return;
+  }
+
+  // Cria um elemento de parágrafo para exibir a resenha
+  var paragrafo = document.createElement("p");
+  paragrafo.textContent = resenha;
+
+// Cria um botão com o símbolo "X" para excluir a resenha
+var botaoExcluir = document.createElement("button");
+botaoExcluir.innerHTML = "&#10006;"; // Código HTML para o símbolo "X"
+botaoExcluir.classList.add("botaoExcluir");
+botaoExcluir.addEventListener("click", function() {
+    divResenha.remove();
+});
+  // Cria um div para conter a resenha e o botão
+  var divResenha = document.createElement("div");
+  divResenha.classList.add("resenha");
+  divResenha.appendChild(paragrafo);
+  divResenha.appendChild(botaoExcluir);
+
+  // Adiciona o div com a resenha e o botão à lista de resenhas salvas
+  document.getElementById("resenhasSalvas").appendChild(divResenha);
+
+  // Limpa a área de texto
+  document.getElementById("resenha").value = "";
+});
